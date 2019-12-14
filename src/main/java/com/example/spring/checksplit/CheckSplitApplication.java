ca45391256fee9b8f57ab2f5d0c01c9593321a00
Bryan Myung <bryanmyung@gmail.com>
2019-12-13 21:46:29 -0800

    final exam

---
+++ src/main/java/com/example/spring/checksplit/CheckSplitApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.checksplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckSplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckSplitApplication.class, args);
+	}
+
+}
